e4bcc9fbc5a9bbb6ce18b1fdaf0baf8ece0df897
ayasaha4 <ayasaha4@in.ibm.com>
2017-12-21 16:24:13 +0530

    adding static variable class

--- src/com/isat/common/StaticVariable.java
+++ src/com/isat/common/StaticVariable.java
@@ -3,6 +3,6 @@ package com.isat.common;
 public class StaticVariable {
 	
 	
-	public static String PROPERTY_FILE = "/ISAT_Incident_tracker/WebContent/resources/properties/eaiservletconfig.properties";
+	public static String PROPERTY_FILE = "/ISAT_Incident_tracker/WebContent/resources/properties/dashboard.properties";
 
 }
--- src/com/isat/controller/HomePageController.java
+++ src/com/isat/controller/HomePageController.java
@@ -1,18 +1,24 @@
 package com.isat.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.isat.pojo.User;
+import com.isat.service.PropertyService;
 
 @Controller
 public class HomePageController {
 
+	@Autowired
+	PropertyService propertyService ;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView showHomePage() {
-
+        
+		propertyService.readValues("error");
 		ModelAndView mav = new ModelAndView("login");
 		mav.addObject("login",new User());
 		/*mav.addObject("register",new User());*/
